5b5a6340ef249fed228b454d18d0339278899640
Fowerus <fowerus@gmail.com>
2022-12-18 21:37:44 +0300

    Create structure of models

---
+++ Projects/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+
+class Task:
+    STATUS_VARIABLES = [
+        (0, 'To do'),
+        (1, 'In progress'),
+        (2, 'Code review'),
+        (3, 'Done')
+    ]
+    name = models.CharField(max_length=100, verbose_name='Name')
+    description = models.TextField(max_length=300, verbose_name='Description')
+
+    status = models.CharField(choices=STATUS_VARIABLES,
+                              default='To do', verbose_name='Status')
+    priority = models.IntegerField(verbose_name='Priority')
+
+    creator = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                                related_name='user_task_creator', verbose_name='Creator')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created_at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Update_at')
+
+    def __str__(self):
+        return f'id: {self.id} | name: {self.name} | ' \
+               f'creator: {self.creator.name} + " {self.creator.surname[0]}."'
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ['-created_at']
+
+
+class Project:
+    name = models.CharField(max_length=100, verbose_name='Name')
+    description = models.TextField(max_length=300, verbose_name='Description')
+
+    lead = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                             related_name='user_project_lead')
+    team = models.ForeignKey('Users.Team', on_delete=models.SET_NULL, null = True, verbose_name='Team')
+
+    done = models.BooleanField(default=False, verbose_name='Done')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created_at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Update_at')
+
+    def __str__(self):
+        return f'id: {self.id} | name: {self.name} | ' \
+               f'lead: {self.lead.name} + " {self.lead.surname[0]}."'
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+        ordering = ['-created_at']
--- Users/models.py
+++ Users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth import get_user_model
 
 
 
@@ -50,7 +51,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name='Avatar', default='users/default-user-image.png')
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created_at')
-    updated_at = models.DateTimeField(auto_now=False, verbose_name='Update_at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Update_at')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ('surname', 'first_name', 'second_name')
@@ -68,17 +69,17 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Team:
     name = models.CharField(max_length=50, verbose_name='Name')
-    lead = models.ForeignKey(User, on_delete=models.SET_NULL,
+    lead = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True,
                              related_name='user_team_lead', verbose_name='Lead')
-    users = models.ManyToManyField(User, related_name='user_team_users',
+    users = models.ManyToManyField(get_user_model(), related_name='user_team_users',
                                    verbose_name='Users')
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created_at')
-    updated_at = models.DateTimeField(auto_now=False, verbose_name='Update_at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Update_at')
 
     def __str__(self):
         return f'id: {self.id} | name: {self.name} | ' \
-               f'name: {self.lead.name} + " {self.lead.surname}[0]"'
+               f'lead: {self.lead.name} + " {self.lead.surname[0]}."'
 
     class Meta:
         verbose_name = 'Team'
