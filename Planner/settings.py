d9cbceb442090b28951a0cf77fb4cf9ef84c946e
Fowerus <fowerus@gmail.com>
2022-12-18 21:03:21 +0300

    Start project v2

--- Planner/settings.py
+++ Planner/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'phonenumber_field',
+
+    'Users',
+    'Projects'
 ]
 
 MIDDLEWARE = [
@@ -115,9 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Project's settings
+AUTH_USER_MODEL = 'Users.User'
--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, surname=None, first_name=None, second_name=None, email=None,
+                     phone=None, password=None, **extra_fields):
+        if email is not None:
+            email = self.normalize_email(email)
+        user = self.model(surname=surname, first_name=first_name,
+                          second_name=second_name, email=email, phone=phone,
+                          **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, surname=None, first_name=None, second_name=None, email=None,
+                    phone=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(surname=surname, first_name=first_name,
+                                 second_name=second_name, email=email,
+                                 phone=phone, password=password, **extra_fields)
+
+    def create_superuser(self, surname=None, first_name=None, second_name=None,
+                         email=None, phone=None, password=None,
+                         **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(surname=surname, first_name=first_name,
+                                 second_name=second_name, email=email, phone=phone,
+                                 password=password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    surname = models.CharField(max_length=50, verbose_name='Surname')
+    first_name = models.CharField(max_length=50, verbose_name='First name')
+    second_name = models.CharField(max_length=50, verbose_name='Second name')
+
+    email = models.EmailField(max_length=60, unique=True, verbose_name='Email')
+    phone = PhoneNumberField(unique=True, blank=True, null=True, verbose_name='Phone')
+
+    avatar = models.ImageField(
+        upload_to='users/', max_length=255, null=False, blank=False,
+        verbose_name='Avatar', default='users/default-user-image.png')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created_at')
+    updated_at = models.DateTimeField(auto_now=False, verbose_name='Update_at')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ('surname', 'first_name', 'second_name')
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'id: {self.id} | email: {self.email} | phone: {self.phone}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['-created_at']
+
+
+class Team:
+    name = models.CharField(max_length=50, verbose_name='Name')
+    lead = models.ForeignKey(User, on_delete=models.SET_NULL,
+                             related_name='user_team_lead', verbose_name='Lead')
+    users = models.ManyToManyField(User, related_name='user_team_users',
+                                   verbose_name='Users')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created_at')
+    updated_at = models.DateTimeField(auto_now=False, verbose_name='Update_at')
+
+    def __str__(self):
+        return f'id: {self.id} | name: {self.name} | ' \
+               f'name: {self.lead.name} + " {self.lead.surname}[0]"'
+
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+        ordering = ['-created_at']
